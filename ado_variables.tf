8fa117c54b78b7fe81e174aedbe168e9fd06930e
sarzali788 <sarzali7_88@hotmail.com>
2023-06-09 22:23:02 +0200

    Update tfsv1

---
+++ ado_variables.tf
@@ -0,0 +1,21 @@
+resource "azuredevops_variable_group" "hawaVB" {
+  project_id   = data.azuredevops_project.tfazlab.id
+  name         = "hawaVB"
+  description  = "This Variable Group should be linked to an Azure Key Vault"
+  allow_access = true
+
+  variable {
+    name  = "SPNPass"
+    value = azuread_application_password.tfazsp.value
+  }
+
+  variable {
+    name  = "SASPass"
+    value = azurerm_storage_account.stg.primary_access_key
+  }
+
+  variable {
+    name  = "VMAdminPass"
+    value = var.VMAdminPass
+  }
+}

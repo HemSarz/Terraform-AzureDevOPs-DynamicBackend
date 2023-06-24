0aac357ac7b0a3b61b6e734dd35681eef49d1057
sarzali788 <sarzali7_88@hotmail.com>
2023-06-24 13:43:33 +0200

    Update tfsv

--- main.tf
+++ main.tf
@@ -47,16 +47,18 @@ resource "azurerm_key_vault" "kv" {
   enabled_for_disk_encryption     = true
   enabled_for_template_deployment = true
   purge_protection_enabled        = false
-}
 
-resource "azurerm_key_vault_access_policy" "Current" {
-  key_vault_id = azurerm_key_vault.kv.id
-  object_id    = data.azurerm_client_config.current.object_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
 
-  key_permissions     = ["Get", "List", "Recover", "Delete", "Purge"]
-  secret_permissions  = ["Get", "List", "Set", "Delete", "Purge"]
-  storage_permissions = ["Get", "List", "Set", "Delete", "Purge"]
+  access_policy {
+    object_id      = data.azurerm_client_config.current.object_id
+    tenant_id      = data.azurerm_client_config.current.tenant_id
+
+
+    key_permissions     = ["Get", "List", "Recover", "Delete"]
+    secret_permissions  = ["Get", "List", "Set", "Delete"]
+    storage_permissions = ["Get", "List", "Set", "Delete"]
+
+  }
 }
 
 resource "azurerm_key_vault_access_policy" "KVAdoServEP" {
@@ -65,7 +67,7 @@ resource "azurerm_key_vault_access_policy" "KVAdoServEP" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   depends_on   = [azurerm_key_vault.kv]
 
- key_permissions     = ["Get", "List", "Recover", "Delete", "Purge"]
+  key_permissions     = ["Get", "List", "Recover", "Delete", "Purge"]
   secret_permissions  = ["Get", "List", "Set", "Delete", "Purge"]
   storage_permissions = ["Get", "List", "Set", "Delete", "Purge"]
 }

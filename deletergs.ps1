5d4c767ed67e877874b9cf103bc2400cc7df6e48
Hemen Sarzali <sarzali7_88@hotmail.com>
2023-07-02 17:46:40 +0200

    UpdateTFsv1

--- deletergs.ps1
+++ deletergs.ps1
@@ -2,8 +2,7 @@ Write-Host "Deleting "backend.tf" & "Terraform folder"..."
 Remove-Item -Path "backend.tf" -Force ; Remove-Item -Path ".terraform" -Force -Recurse
 
 Write-Host "Deleting Azure resource groups..."
-az group delete --name networkwatcherrg --yes
-az group delete --name tfaz-rg-aad --yes
+az group delete --name rg01hh --yes
 
 Start-Sleep -Seconds 2
 
